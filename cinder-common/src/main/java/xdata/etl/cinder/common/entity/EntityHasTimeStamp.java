8d268fc460e136b48441f8a75c2862e42b6cf1df
xuehuihe <xuehuihe.java@gmail.com>
2013-09-05 18:46:59 +0800

    sd

---
+++ cinder-common/src/main/java/xdata/etl/cinder/common/entity/EntityHasTimeStamp.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2013 BEIJING UNION VOOLE TECHNOLOGY CO., LTD
+ */
+package xdata.etl.cinder.common.entity;
+
+import java.util.Date;
+
+/**
+ * @author XuehuiHe
+ * @date 2013年9月5日
+ */
+public interface EntityHasTimeStamp {
+	Date getLastUpdateTimeStamp();
+
+	Date getCreateTime();
+
+	void setCreateTime(Date createTime);
+
+	void setLastUpdateTimeStamp(Date lastUpdateTimeStamp);
+
+	String getCreateTimePropertyName();
+}
