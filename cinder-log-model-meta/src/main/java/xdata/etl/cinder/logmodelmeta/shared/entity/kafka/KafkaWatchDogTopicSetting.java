c22ddc536bbfab19773f443b1925415b427a16a2
xuehuihe <xuehuihe.java@gmail.com>
2013-10-08 17:46:07 +0800

    sd

--- cinder-log-model-meta/src/main/java/xdata/etl/cinder/logmodelmeta/shared/entity/kafka/KafkaWatchDogTopicSetting.java
+++ cinder-log-model-meta/src/main/java/xdata/etl/cinder/logmodelmeta/shared/entity/kafka/KafkaWatchDogTopicSetting.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -39,6 +40,7 @@ public class KafkaWatchDogTopicSetting extends LogModelBase {
 	}
 
 	@ManyToOne(optional = false)
+	@JoinColumn(updatable = false)
 	public KafkaTopic getTopic() {
 		return topic;
 	}
