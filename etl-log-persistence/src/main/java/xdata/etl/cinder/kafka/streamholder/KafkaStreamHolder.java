f1007223062deb2e9c7ee3d42da6ca38584962b0
xuehuihe <xuehuihe.java@gmail.com>
2013-10-16 18:55:05 +0800

    sd

---
+++ etl-log-persistence/src/main/java/xdata/etl/cinder/kafka/streamholder/KafkaStreamHolder.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2013 BEIJING UNION VOOLE TECHNOLOGY CO., LTD
+ */
+package xdata.etl.cinder.kafka.streamholder;
+
+import kafka.consumer.KafkaStream;
+import kafka.message.Message;
+import xdata.etl.cinder.logmodelmeta.shared.entity.kafka.KafkaTopic;
+
+/**
+ * @author XuehuiHe
+ * @date 2013年10月16日
+ */
+public abstract class KafkaStreamHolder implements Runnable {
+	private final KafkaStream<Message> stream;
+	private final KafkaTopic topic;
+
+	protected KafkaStreamHolder(KafkaStream<Message> stream,
+			KafkaTopic topic) {
+		this.stream = stream;
+		this.topic = topic;
+	}
+
+	public KafkaStream<Message> getStream() {
+		return stream;
+	}
+
+	public KafkaTopic getTopic() {
+		return topic;
+	}
+}
---
+++ etl-log-persistence/src/main/java/xdata/etl/cinder/kafka/streamholder/KafkaStreamHolderFactory.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2013 BEIJING UNION VOOLE TECHNOLOGY CO., LTD
+ */
+package xdata.etl.cinder.kafka.streamholder;
+
+import kafka.consumer.KafkaStream;
+import kafka.message.Message;
+import xdata.etl.cinder.logmodelmeta.shared.entity.kafka.KafkaTopic;
+
+/**
+ * @author XuehuiHe
+ * @date 2013年10月16日
+ */
+public interface KafkaStreamHolderFactory {
+	public KafkaStreamHolder createHolder(KafkaStream<Message> stream,
+			KafkaTopic topic);
+}
